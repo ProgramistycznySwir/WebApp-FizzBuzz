bf39995fe1dd96f130d2b807a7ad45b31164ec34
ProgramistycznyŚwir <programistycznyswir@gmail.com>
2021-03-25 13:58:26 +0100

    Dodaj pliki projektów.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApp_FizzBuzz.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [Display(Name = "FizzBuzz")]
+        [Range(1, 1000, ErrorMessage= "Value has to be in range from 1 to 1000!")]
+        public int? input { get; set; }
+
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            try
+            {
+                input = HttpContext.Session.GetInt32("input");
+            }
+            catch(Exception e)
+            { 
+                // Ignore
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+            //ModelState.TryGetValue("input", out input);
+            HttpContext.Session.SetInt32("input", input.Value);
+            return RedirectToPage("./OstatnioWyszukiwane");
+
+            if (ModelState.IsValid)
+            {
+                HttpContext.Session.SetString("SessionAddress",
+                    JsonConvert.SerializeObject(input));
+                return RedirectToPage("./OstatnioWyszukiwane");
+            }
+            return Page();
+        }
+    }
+}
