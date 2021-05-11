ece52386182d8a2ac5ea4c3d4fd9d011492c195a
ProgramistycznyŚwir <programistycznyswir@gmail.com>
2021-05-11 23:07:03 +0200

    Scaffolded some authority >w<

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp_FizzBuzz.Data;
+
+[assembly: HostingStartup(typeof(WebApp_FizzBuzz.Areas.Identity.IdentityHostingStartup))]
+namespace WebApp_FizzBuzz.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<Accounts>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AccountsConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<Accounts>();
+            });
+        }
+    }
+}
